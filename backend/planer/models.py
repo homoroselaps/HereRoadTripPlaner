a609929dca2d5dea5b759c615a59a4baf48768ba
Florian Richter <florian.richter@student.hpi.uni-potsdam.de>
2015-06-12 13:56:15 +0200

    create test app and view

--- backend/planer/models.py
+++ backend/planer/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=40)
+    x = models.IntegerField('Coordinate X')
+    y = models.IntegerField('Coordinate Y')
+    def __str__(self):
+        return self.name
+
+class UserSettings(models.Model):
+    sessionId = models.IntegerField()
+    drivingPerDay = models.IntegerField('in hours')
+    totalDays = models.IntegerField()
+    def __str__(self):
+        return self.sessionId
+
+class Route(models.Model):
+    city = models.ForeignKey(City)
+    settings = models.ForeignKey(UserSettings)
+    def __str__(self):
+        return self.city
+
