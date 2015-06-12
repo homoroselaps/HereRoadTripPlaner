5f67cb78144e7884a05c58da11df9be699036be2
Florian Richter <florian.richter@student.hpi.uni-potsdam.de>
2015-06-12 12:39:22 +0200

    adjust .pro and gitignore, fix main.cpp but fossa is broken

--- backend/RoadTripPlanner/RoadTripPlanner.pro
+++ backend/RoadTripPlanner/RoadTripPlanner.pro
@@ -1,13 +1,18 @@
 TEMPLATE = app
-CONFIG += console
+CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 
-SOURCES += source/main.cpp \
-    source/fossa/fossa.c
-
 include(deployment.pri)
 qtcAddDeployment()
+
+INCLUDEPATH += \
+    source \
+    source/fossa
+
+SOURCES += \
+    source/main.cpp \
+    source/fossa/fossa.c
 
 HEADERS += \
     source/fossa/fossa.h
--- backend/RoadTripPlanner/source/main.cpp
+++ backend/RoadTripPlanner/source/main.cpp
@@ -1,37 +1,37 @@
-#include "source/fossa/fossa.h"
+#include <fossa.h>
 
 static void ev_handler(struct ns_connection * nc, int ev, void *)
 {
-  struct iobuf * io = &nc->recv_iobuf;
+    struct mbuf * mbuf = &nc->recv_mbuf;
 
-  switch (ev)
-  {
-    case NS_RECV:
-      // This event handler implements simple TCP echo server
-      ns_send(nc, io->buf, io->len);  // Echo received data back
-      iobuf_remove(io, io->len);      // Discard data from recv buffer
-      break;
-    default:
-      break;
-  }
+    switch (ev)
+    {
+        case NS_RECV:
+            // This event handler implements simple TCP echo server
+            ns_send(nc, mbuf->buf, mbuf->len);  // Echo received data back
+            mbuf_remove(mbuf, mbuf->len);      // Discard data from recv buffer
+            break;
+        default:
+            break;
+    }
 }
 
-int main(void)
+int main()
 {
-  struct ns_mgr mgr;
+    struct ns_mgr mgr;
 
-  ns_mgr_init(&mgr, NULL);
+    ns_mgr_init(&mgr, nullptr);
 
-  // Note that many connections can be added to a single event manager
-  // Connections can be created at any point, e.g. in event handler function
-  ns_bind(&mgr, "1234", ev_handler, NULL);
+    // Note that many connections can be added to a single event manager
+    // Connections can be created at any point, e.g. in event handler function
+    ns_bind(&mgr, "1234", ev_handler);
 
-  for (;;)
-  {
-    ns_mgr_poll(&mgr, 1000);
-  }
+    for (;;)
+    {
+        ns_mgr_poll(&mgr, 1000);
+    }
 
-  ns_mgr_free(&mgr);
-  return 0;
+    ns_mgr_free(&mgr);
+    return 0;
 }
 
